5ce72354d965fa5b23fa150027668a882ee30833
lk2322 <ll2322@y.ru>
2020-02-09 18:17:36 +0600

    hdhdh

--- bot.py
+++ bot.py
@@ -1,5 +1,15 @@
 import sqlite3
 import telebot
 import os
-bot = telebot.TeleBot(123)
-print(bot)
+from dotenv import load_dotenv
+load_dotenv()
+token = os.getenv('API_KEY')
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(func=lambda m: True)
+def history(message):
+    bot.reply_to(message, message.from_user.id)
+    bot.reply_to(message, message.from_user.first_name)
+
+
+bot.polling()
